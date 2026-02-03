5e78434b45943943ab703c3e28194971ec40d11f
Eugenio Guzman <eugenio.guzman@hp.com>
2026-02-03 12:19:08 -0600

    second test

--- test.py
+++ test.py
@@ -2,3 +2,16 @@ print("Hello world!")
 
 for i in range(5): 
     print(i)
+
+def printNames(): 
+    names = ["Eugenio", "HP", "Tec de Mty"]
+    for name in names: 
+        print(name)
+
+def printNames(x):
+    for name in x: 
+        print(name)
+
+states = ["Ags", "Gdl", "Qro"]
+
+printNames(states)
